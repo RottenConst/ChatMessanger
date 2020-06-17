2870d5f343b26e59392cbae8fd057d29514ae999
konstantin <konstload@gmail.com>
2020-06-17 22:43:20 +0300

    Cache layer. logout, get account, save account

--- app/src/main/java/com/konstload/chatmessanger/cache/AccountCacheImpl.kt
+++ app/src/main/java/com/konstload/chatmessanger/cache/AccountCacheImpl.kt
@@ -1,6 +1,7 @@
 package com.konstload.chatmessanger.cache
 
 import com.konstload.chatmessanger.data.account.AccountCache
+import com.konstload.chatmessanger.domain.account.AccountEntity
 import com.konstload.chatmessanger.domain.type.Either
 import com.konstload.chatmessanger.domain.type.None
 import com.konstload.chatmessanger.domain.type.Failure
@@ -17,5 +18,17 @@ class AccountCacheImpl @Inject constructor(private val prefsManager: SharedPrefs
 
     override fun getToken(): Either<Failure, String> {
         return prefsManager.getToken()
+    }
+
+    override fun logout(): Either<Failure, None> {
+        return prefsManager.removeAccount()
+    }
+
+    override fun getCurrentAccount(): Either<Failure, AccountEntity> {
+        return prefsManager.getAccount()
+    }
+
+    override fun saveAccount(account: AccountEntity): Either<Failure, None> {
+        return prefsManager.saveAccount(account)
     }
 }
